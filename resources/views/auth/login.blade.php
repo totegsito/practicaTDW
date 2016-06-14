0ce945c4e260449ca635840eb55f0f11142d7697
totegsito <pacoquesmun@gmail.com>
2016-06-14 10:17:35 +0200

    Error en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,15 +10,15 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="email" value="{{ old('name') }}">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
