0b8993293bea4cd7d461fcba89b1881967880cb0
Francisco Quesada <totegsito@users.noreply.github.com>
2016-06-29 17:52:22 +0200

    Revert "Features"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,7 +49,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin' => \App\Http\Middleware\RolMiddleware::class,
-        'user' => \App\Http\Middleware\UserMiddleware::class,
     ];
 }
