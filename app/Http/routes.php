b9b0c71b5067910099f26ce1be995ef68daddf3f
totegsito <pacoquesmun@gmail.com>
2016-06-29 12:27:57 +0200

    Rutas protegidas para evitar que usuarios entren como admin y viceversa

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,78 +3,51 @@
 
 
 
-Route::group(['middleware'=>'auth.basic'], function(){
-   Route::group(['prefix'=>'api'], function(){
-       Route::resource('users', 'UsersController', ['except'=>['edit', 'create']]);
-       Route::options('users', 'UsersController@options');
-       Route::put('users/password/{id}', 'UsersController@changePassword');
-       Route::get('users/reservations/{id}', "ReservationsController@getReservationsByUserId");
-       Route::resource('courts', 'CourtsController', ['except'=>['edit', 'create']]);
-       Route::options('courts', 'CourtsController@options');
-       Route::group(['prefix'=>'reservations'], function (){
-           Route::resource('/', 'ReservationsController', ['except'=>['edit', 'create']]);
-         Route::get('/user/{users_id}/{reservation_date?}', 'ReservationsController@getReservationsByUserId');
-       });
-   });
-
-   Route::group(['prefix'=>'admin'], function (){
-       Route::get('/', function (){
-           return view('admin');
-       });
-       Route::get('users', function (){
-           return view('layouts.admin.users');
-       });
-       Route::get('courts', function (){
-           return view('layouts.admin.courts');
-       });
-       Route::get('profile', function (){
-           return view('layouts.profile', ["id"=>Auth::user()->id]);
-       });
-       Route::get('reservations', function (){
-           return view('layouts.admin.reservations');
-       });
-   }) ;
-
-    Route::group(['prefix'=>'user'],function (){
-        Route::get('/', function (){
-            return view('layouts.user');
-        });
-    });
-});;
-
 Route::auth();
 
 Route::get('/', 'HomeController@index');
+Route::group(['middleware'=>['api'],'prefix' => 'api'], function () {
+    Route::resource('users', 'UsersController', ['except' => ['edit', 'create']]);
+    Route::options('users', 'UsersController@options');
+    Route::put('users/password/{id}', 'UsersController@changePassword');
+    Route::get('users/reservations/{id}', "ReservationsController@getReservationsByUserId");
+    Route::resource('courts', 'CourtsController', ['except' => ['edit', 'create']]);
+    Route::options('courts', 'CourtsController@options');
+    Route::resource('reservations', 'ReservationsController', ['except' => ['edit', 'create']]);
+    Route::get('reservations/user/{users_id}/{reservation_date?}', 'ReservationsController@getReservationsByUserId');
+});
+Route::group(['middleware' => 'auth' ], function () {
 
-
-/*Route::group(['middleware'=>'auth.basic', 'prefix'=>'admin'], function (){
-        Route::get('/', function (){
+    Route::group(['middleware' => 'admin', 'prefix' => 'admin'], function () {
+        Route::get('/', function () {
             return view('admin');
         });
-        Route::get('users', function (){
-            return view('layouts.users');
+        Route::get('users', function () {
+            return view('layouts.admin.users');
         });
-        Route::get('courts', function (){
-            return view('layouts.courts');
+        Route::get('courts', function () {
+            return view('layouts.admin.courts');
         });
-        Route::get('profile', function (){
-            return view('layouts.profile', ["id"=>Auth::user()->id]);
+        Route::get('profile', function () {
+            return view('layouts.profile', ["id" => Auth::user()->id]);
         });
-});*/
+        Route::get('reservations', function () {
+            return view('layouts.admin.reservations');
+        });
 
 
+    });
 
-/*Route::group(['middleware'=>'auth.basic', 'prefix'=>"api"],function (){
-    Route::resource('users', 'UsersController', ['except'=>['edit', 'create']]);
-    Route::options('users', 'UsersController@options');
-    Route::resource('courts', 'CourtsController', ['except'=>['edit', 'create']]);
-    Route::options('courts', 'CourtsController@options');
-    Route::resource('reservations', 'ReservationsController', ['except'=>['edit', 'create']]);
-});*/
-
-
-/*
-Route::get('admin', 'AdminController@index');
-Route::get('admin/users', 'AdminController@users');
-Route::get('admin/courts', 'AdminController@courts');
-Route::get('admin/profile', 'AdminController@profile');*/
+    Route::group(['middleware' => 'user', 'prefix' => 'user'], function () {
+        Route::get('/', function () {
+            if (Auth::user()->enabled == 1) {
+                return view('layouts.user.user');
+            } else {
+                return view('layouts.user.usernotenabled');
+            }
+        });
+        Route::get('profile', function () {
+            return view('layouts.profile', ["id" => Auth::user()->id]);
+        });
+    });
+});;
